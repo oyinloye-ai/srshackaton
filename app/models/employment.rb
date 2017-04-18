7d477f8c5fa3a33a711f1e192aecefc1a139ea06
oyinloye-ai <ayodeji.oyinloye@meltwater.org>
2017-04-18 10:44:50 +0000

    all models have scores

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -5,23 +5,28 @@ class Employment < ApplicationRecord
 
   def employment_score
 		score = 0
-		if amount < 500 
-			score = 50
-		elsif amount < 1000
-			score = 80
-		elsif amount < 20000
-			score = 100
-		elsif amount < 300
-			score = 150
-		elsif amount < 50000
-			score = 200
-		elsif amount < 80000
-			score = 300
-		elsif amount < 100000
-			score = 500
+		if average_income != nil
+			if average_income < 500 
+				score = 50
+			elsif average_income < 1000
+				score = 80
+			elsif average_income < 20000
+				score = 100
+			elsif average_income < 300
+				score = 150
+			elsif average_income < 50000
+				score = 200
+			elsif average_income < 80000
+				score = 300
+			elsif average_income < 100000
+				score = 500
+			else
+				score = 100
+			end
 		else
-			score = 100
-		end			
+			score = 20
+		end
+
 		score = (score * 20/100000) * 800/100
 	end
 end
